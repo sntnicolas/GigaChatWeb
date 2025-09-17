a834fa200321a9031fc29ae46e1f2325bf43a31f
sntNicolAs <sntnicolas@gmail.com>
2025-09-17 15:07:18 +0300

    рефакторинг

--- qa_agent/llm.py
+++ qa_agent/llm.py
@@ -1,5 +1,16 @@
+# llm.py
 from langchain_gigachat.chat_models import GigaChat
+from .config import GIGACHAT_MODEL, GIGACHAT_SCOPE, GIGACHAT_CREDENTIALS, VERIFY_SSL
 
-llm = GigaChat(model="GigaChat-2-Max", top_p=0, timeout=120)
-response = llm.invoke("Кто тебя создал?")
-print(response.content)
+def get_llm() -> GigaChat:
+    return GigaChat(
+        model=GIGACHAT_MODEL,
+        credentials=GIGACHAT_CREDENTIALS,
+        scope=GIGACHAT_SCOPE,
+        verify_ssl_certs=VERIFY_SSL
+    )
+
+if __name__ == "__main__":
+    llm = get_llm()
+    response = llm.invoke("Кто тебя создал?")
+    print(response.content)
